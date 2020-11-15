9db25c8c60c289fc8ff078a682b335d9620e7ed7
Kagachi21 <fairytail058@gmail.com>
2020-11-15 23:36:00 +0700

    wait

--- application/views/content/pegawai/Admin/dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('adminlte/h_admin.php') ?>
-<?php $this->load->view("adminlte/d_admin.php") ?>
-    
-<?php $this->load->view("adminlte/s_admin.php") ?>
-<?php $this->load->view("adminlte/f_admin.php") ?>
--- application/views/content/pegawai/Dosen/dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('dosenlte/h_dosen_teknisi.php') ?>
-<?php $this->load->view("dosenlte/d_dosen_teknisi.php") ?>
-    
-<?php $this->load->view("dosenlte/s_dosen_teknisi.php") ?>
-<?php $this->load->view("dosenlte/f_dosen_teknisi.php") ?>
